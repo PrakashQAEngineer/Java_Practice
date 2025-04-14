d8053947aee1cfdf7f3ef55cb720f13fcd0a17a1
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-04-14 07:56:08 +0530

    Add files via upload

---
+++ Descending_arrAY.java
@@ -0,0 +1,31 @@
+
+public class Descending_arrAY 
+{
+	public static void main(String args[])
+	{
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		int temp=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1; j<arr.length;j++)
+			{
+				if(arr[i]<arr[j])
+						{
+					      temp = arr[i];
+					      arr[i] = arr[j];
+					      arr[j] = temp;
+			 			}
+				
+			}
+			
+			
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]);
+			
+		}
+		//System.out.println("    The 2nd largetst number is: "+arr[1]);
+	}
+
+}
