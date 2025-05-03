05c94f5775aec876aaf9a19f36bc4648e8eb6afd
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-05-03 11:20:06 +0530

    OverRiding_1.java

--- OverRiding_1.java
+++ OverRiding_1.java
@@ -6,6 +6,11 @@ class Override
 	{
 		System.out.println("This is the Override display method");
 	}
+	
+	public void para(String str)
+	{
+		System.out.println("This will be with the Parent String method");
+	}
 }
 public class OverRiding_1 extends Override
 {
@@ -13,13 +18,21 @@ public class OverRiding_1 extends Override
 	{
 	  System.out.println("This is the main method class");	
 	}
+	
+	public void para(String str)
+	{
+		System.out.println("This will be with the child string method");
+	}
+	
 	public static void main(String args[])
 	{
 		Override ov1 = new Override();
 		ov1.display(); // this will call the parent class display method
+		ov1.para("Prakash");
 			
 		OverRiding_1 ov2 = new OverRiding_1();
 		ov2.display(); // this will call the child class display method
+		ov2.para("Prakash");
 		
 	}
 
