6a83715efff81343e52331ecca8b804adca2cb13
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-08-03 17:48:00 +0530

    Add files via upload

---
+++ StringSort.java
@@ -0,0 +1,28 @@
+
+public class StringSort 
+{
+	public static void main(String args[])
+	{
+		String[]  arr = {"Prakash", "Jyoti", "SurajMukhi", "Gulu", "KUKU", "Prakash"};
+		String temp = "";
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr.length-1-i;j++)
+			{
+				if(arr[j].compareToIgnoreCase(arr[j+1])>0)
+				{
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+					
+			}
+		}
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+
+}
