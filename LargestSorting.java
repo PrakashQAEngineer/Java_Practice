17ffe0d5bd67850c7c268a093e045d7d64506f41
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-08-04 07:33:10 +0530

    LargestSorting.java

--- LargestSorting.java
+++ LargestSorting.java
@@ -5,6 +5,7 @@ public class LargestSorting
 	{
 		int[] arr = {67,55,10,1,89,100};
 		int temp;
+		int k=3; //it is being used for that largest number. 
 		for(int i=0;i<arr.length-1;i++)
 		{
 			for(int j=i+1; j<arr.length;j++)
@@ -22,6 +23,9 @@ public class LargestSorting
 		{
 			System.out.println(arr[i]);
 		}	
+		
+		//using for that largest number.
+		System.out.println("The Third largest number is: "+arr[k-1]);
 	}
 
 }
