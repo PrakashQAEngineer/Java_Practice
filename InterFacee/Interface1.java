e23980b139cae390ac6313724c132d7874c0728c
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-09-14 18:32:49 +0530

    Add files via upload

---
+++ InterFacee/Interface1.java
@@ -0,0 +1,51 @@
+package DataAbstraction;
+
+interface i1
+{
+	void show();  // complier will auto add the public abstract
+	public abstract void data();
+	
+	public static void meth()
+	{
+		System.out.println("here we can define with the use of static");
+	}
+}
+
+interface i2
+{
+	public static void anima()
+	{
+		System.out.println("This is the animal");
+	}
+	
+	public abstract void calling();
+}
+
+public class Interface1 implements i1 ,i2
+{
+	
+	public void show()
+	{
+		System.out.println("This will display the data");
+	}
+	
+	public void calling()
+	{
+		System.out.println("This is the calling odf the 2nd interface and this is also the example of the multiple inheritance");
+	}
+	
+	public void data()
+	{
+		System.out.println("This is the data");
+	}
+	
+	public static void main(String args[])
+	{
+		Interface1 i = new Interface1();
+		i.data();
+		i.show();
+		i1.meth();  // we can call directly because the method is static
+		i.calling();  //this is the multiple inheritance
+	}
+
+}
