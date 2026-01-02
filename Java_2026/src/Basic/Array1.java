f5294dfe2ff0bae4fe4b0930ede941814f82f7fd
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2026-01-03 00:38:38 +0530

    Add files via upload

---
+++ Java_2026/src/Basic/Array1.java
@@ -0,0 +1,15 @@
+package Basic;
+
+public class Array1 
+{
+	public static void main(String args[])
+	{
+		int[] arr1 = {10,20,30,40};
+		
+		for(int i:arr1)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
